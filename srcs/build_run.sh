5ca61c7cff3d26680660a482033229f00b1707db
edithturn <edithpuclla20@gmail.com>
2020-12-01 23:39:27 -0800

    removing instruction to remove all containers

--- srcs/build_run.sh
+++ srcs/build_run.sh
@@ -6,8 +6,6 @@ docker build -t ft_server .
 
 echo ""
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~ RUN ~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-# Killing previous containers
-docker rm -f $(docker ps -qa)
 # Creating the container
 docker run --name ft_server -it  -p 80:80 -p 443:443 ft_server
 
